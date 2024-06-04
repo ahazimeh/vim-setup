55dab13123c9b7857dd0049b580dfb187ee05aa6
ahazimeh01 <ali.h@echovalley.io>
2024-06-04 18:13:46 +0300

    fix neotest and added neo tree

---
+++ lua/theprimeagen/lazy/neo-tree.lua
@@ -0,0 +1,11 @@
+return {
+  "kyazdani42/nvim-tree.lua",
+  version = "*",
+  lazy = false,
+  dependencies = {
+    "kyazdani42/nvim-web-devicons",
+  },
+  config = function()
+    require("nvim-tree").setup {}
+  end,
+}
