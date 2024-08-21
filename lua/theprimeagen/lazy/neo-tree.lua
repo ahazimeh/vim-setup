98fc5108ce26b3902cf970925b22a5146c3dffa3
Ali Hazimeh <hazimeh95@gmail.com>
2024-08-21 23:03:47 +0300

    removed init.lua and added autoNavigate for dir change

--- lua/theprimeagen/lazy/neo-tree.lua
+++ lua/theprimeagen/lazy/neo-tree.lua
@@ -6,12 +6,20 @@ return {
     "kyazdani42/nvim-web-devicons",
   },
   config = function()
-    require("nvim-tree").setup {}
-      vim.api.nvim_set_keymap(
-        'n',         -- normal mode
-        '<C-t>',     -- keybinding
-        ':NvimTreeToggle<CR>', -- command to toggle nvim-tree
-        { noremap = true, silent = true } -- options
-      )
+    require("nvim-tree").setup {
+      sync_root_with_cwd = true,
+      respect_buf_cwd = true,
+      update_focused_file = {
+        enable = true,
+        update_root = true
+      },
+    }
+    
+    vim.api.nvim_set_keymap(
+      'n',         -- normal mode
+      '<C-t>',     -- keybinding
+      ':NvimTreeToggle<CR>', -- command to toggle nvim-tree
+      { noremap = true, silent = true } -- options
+    )
   end,
 }
