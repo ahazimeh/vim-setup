7e3859e0551a877814e9d674380437765c3a5d61
Ali Hazimeh <hazimeh95@gmail.com>
2024-08-29 00:52:04 +0300

    comment

--- lua/theprimeagen/lazy/commentary.lua
+++ lua/theprimeagen/lazy/commentary.lua
@@ -1,53 +1,35 @@
-return {
-  {
-    "tpope/vim-commentary",
-    event = "VeryLazy",
-    config = function()
-      -- Function to determine if cursor is inside JSX
-      local function is_inside_jsx()
-        local buf = vim.api.nvim_get_current_buf()
-        local row, col = unpack(vim.api.nvim_win_get_cursor(0))
-        row = row - 1  -- API is 0-indexed
-
-        local parser = vim.treesitter.get_parser(buf, "tsx")
-        if not parser then return false end
-
-        local tree = parser:parse()[1]
-        local root = tree:root()
-
-        local node = root:named_descendant_for_range(row, col, row, col)
-        while node do
-          if node:type() == "jsx_element" or node:type() == "jsx_fragment" then
-            return true
-          end
-          node = node:parent()
-        end
-        return false
-      end
-
-      -- Function to set appropriate comment string
-      local function set_comment_string()
-        if is_inside_jsx() then
-          vim.bo.commentstring = '{/* %s */}'
-        else
-          vim.bo.commentstring = '// %s'
-        end
-      end
-
-      -- Set up autocommand to update comment string before commenting
-      vim.api.nvim_create_autocmd("FileType", {
-        pattern = { "javascript", "javascriptreact", "typescript", "typescriptreact" },
-        callback = function()
-          vim.api.nvim_create_autocmd("CursorMoved", {
-            buffer = 0,
-            callback = set_comment_string
-          })
-        end
-      })
-    end,
-    keys = {
+return {-- tpope/vim-commentary
+	"numToStr/Comment.nvim",
+	event = "BufReadPost",
+	dependencies = {
+		"JoosepAlviste/nvim-ts-context-commentstring",
+	},
+	config = function()
+		require("Comment").setup({
+			padding = true,
+			sticky = true,
+			toggler = {
+				line = "<C-_>",
+				block = "<C-_>",
+				--[[ line = "gcc",
+				block = "gbc", ]]
+			},
+			opleader = {
+				line = "<C-_>",
+				block = "<C-_>",
+				--[[ line = "gc",
+				block = "gb", ]]
+			},
+			mappings = {
+				basic = true,
+				extra = true,
+				extended = false,
+			},
+			pre_hook = require("ts_context_commentstring.integrations.comment_nvim").create_pre_hook(),
+		})
+	end,
+    --[[ keys = {
       { "<C-_>", "<Plug>CommentaryLine", mode = "n", desc = "Comment line" },
       { "<C-_>", "<Plug>Commentary", mode = "v", desc = "Comment selection" },
-    },
-  },
+    }, ]]
 }
