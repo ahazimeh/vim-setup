f585ea1cb835b86934fa1d140a7717471abf520b
Ali Hazimeh <hazimeh95@gmail.com>
2024-09-25 10:22:58 +0300

    remove prehook

--- lua/theprimeagen/lazy/commentary.lua
+++ lua/theprimeagen/lazy/commentary.lua
@@ -2,7 +2,7 @@ return {-- tpope/vim-commentary
 	"numToStr/Comment.nvim",
 	event = "BufReadPost",
 	dependencies = {
-		"JoosepAlviste/nvim-ts-context-commentstring",
+		--[[ "JoosepAlviste/nvim-ts-context-commentstring", ]]
 	},
 	config = function()
 		require("Comment").setup({
@@ -25,7 +25,7 @@ return {-- tpope/vim-commentary
 				extra = true,
 				extended = false,
 			},
-			pre_hook = require("ts_context_commentstring.integrations.comment_nvim").create_pre_hook(),
+			--[[ pre_hook = require("ts_context_commentstring.integrations.comment_nvim").create_pre_hook(), ]]
 		})
 	end,
     --[[ keys = {
