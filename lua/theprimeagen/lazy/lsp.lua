30d0a4fd641acab949ad3cd2e3f3cafa517a3f76
Ali Hazimeh <hazimeh95@gmail.com>
2025-01-02 12:45:34 +0200

    volar

--- lua/theprimeagen/lazy/lsp.lua
+++ lua/theprimeagen/lazy/lsp.lua
@@ -17,34 +17,43 @@ return {
         local cmp = require('cmp')
         local cmp_lsp = require("cmp_nvim_lsp")
 
-vim.api.nvim_create_user_command("RestartVolarLSPFast", function()
-  -- Stop all instances of Volar LSP
-  for _, client in ipairs(vim.lsp.get_active_clients()) do
-    if client.name == "volar" then
-      vim.cmd("LspStop volar")
-      vim.lsp.stop_client(client.id)
-    end
-  end
+        vim.api.nvim_create_user_command("RestartVolarLSPFast", function()
+            -- Stop all instances of Volar LSP
+            for _, client in ipairs(vim.lsp.get_active_clients()) do
+                if client.name == "volar" then
+                    vim.lsp.stop_client(client.id)
+                end
+            end
 
-  -- Brief delay before restart
-  vim.defer_fn(function()
-    -- Start Volar LSP if it's not already running
-    if not vim.tbl_contains(vim.lsp.get_active_clients(), "volar") then
-      vim.cmd("LspStart volar")
-    end
-  end, 100)
-end, {})
+            -- Brief delay before restart
+            vim.defer_fn(function()
+                -- Only restart Volar if it's not already running
+                local clients = vim.lsp.get_active_clients()
+                local volar_running = false
+                for _, client in ipairs(clients) do
+                    if client.name == "volar" then
+                        volar_running = true
+                        break
+                    end
+                end
 
--- Automatically restart Volar LSP after saving or deleting a .vue file
-vim.api.nvim_create_autocmd({ "BufWritePost", "BufDelete" }, {
-  pattern = "*.vue",
-  callback = function()
-    -- Only restart LSP if the current buffer is valid
-    if vim.api.nvim_buf_is_valid(vim.api.nvim_get_current_buf()) then
-      vim.cmd("RestartVolarLSPFast")
-    end
-  end,
-})
+                if not volar_running then
+                    vim.cmd("LspStart volar")
+                end
+            end, 100)
+        end, {})
+
+        -- Automatically restart Volar LSP after saving or deleting a .vue file
+        vim.api.nvim_create_autocmd({ "BufWritePost", "BufDelete" }, {
+            pattern = "*.vue",
+            callback = function()
+                -- Only restart LSP if the current buffer is valid
+                local current_buf = vim.api.nvim_get_current_buf()
+                if vim.api.nvim_buf_is_valid(current_buf) then
+                    vim.cmd("RestartVolarLSPFast")
+                end
+            end,
+        })
 
 
         -- Debugging: Ensure cmp_lsp.default_capabilities() is a valid table
@@ -95,56 +104,56 @@ vim.api.nvim_create_autocmd({ "BufWritePost", "BufDelete" }, {
                         }
                     }
                 end,
-        ["volar"] = function()
-          require("lspconfig").volar.setup({
-            capabilities = capabilities,
-            -- NOTE: Uncomment to enable volar in file types other than vue.
-            -- (Similar to Takeover Mode)
+         ["volar"] = function()
+           require("lspconfig").volar.setup({
+             capabilities = capabilities,
+             -- NOTE: Uncomment to enable volar in file types other than vue.
+             -- (Similar to Takeover Mode)
 
-            -- filetypes = { "vue", "javascript", "typescript", "javascriptreact", "typescriptreact", "json" },
+             -- filetypes = { "vue", "javascript", "typescript", "javascriptreact", "typescriptreact", "json" },
 
-            -- NOTE: Uncomment to restrict Volar to only Vue/Nuxt projects. This will enable Volar to work alongside other language servers (tsserver).
+             -- NOTE: Uncomment to restrict Volar to only Vue/Nuxt projects. This will enable Volar to work alongside other language servers (tsserver).
 
-            -- root_dir = require("lspconfig").util.root_pattern(
-            --   "vue.config.js",
-            --   "vue.config.ts",
-            --   "nuxt.config.js",
-            --   "nuxt.config.ts"
-            -- ),
-            init_options = {
-              vue = {
-                hybridMode = false,
-              },
-              -- NOTE: This might not be needed. Uncomment if you encounter issues.
+             -- root_dir = require("lspconfig").util.root_pattern(
+             --   "vue.config.js",
+             --   "vue.config.ts",
+             --   "nuxt.config.js",
+             --   "nuxt.config.ts"
+             -- ),
+             init_options = {
+               vue = {
+                 hybridMode = false,
+               },
+               -- NOTE: This might not be needed. Uncomment if you encounter issues.
 
-              -- typescript = {
-              --   tsdk = vim.fn.getcwd() .. "/node_modules/typescript/lib",
-              -- },
-            },
-          })
-        end,
+               -- typescript = {
+               --   tsdk = vim.fn.getcwd() .. "/node_modules/typescript/lib",
+               -- },
+             },
+           })
+         end,
 
-        ["tsserver"] = function()
-          local mason_packages = vim.fn.stdpath("data") .. "/mason/packages"
-          local volar_path = mason_packages .. "/vue-language-server/node_modules/@vue/language-server"
+         ["tsserver"] = function()
+           local mason_packages = vim.fn.stdpath("data") .. "/mason/packages"
+           local volar_path = mason_packages .. "/vue-language-server/node_modules/@vue/language-server"
 
-          require("lspconfig").tsserver.setup({
-            capabilities = capabilities,
-            -- NOTE: To enable Hybrid Mode, change hybrideMode to true above and uncomment the following filetypes block.
-            -- WARN: THIS MAY CAUSE HIGHLIGHTING ISSUES WITHIN THE TEMPLATE SCOPE WHEN TSSERVER ATTACHES TO VUE FILES
+           require("lspconfig").tsserver.setup({
+             capabilities = capabilities,
+             -- NOTE: To enable Hybrid Mode, change hybrideMode to true above and uncomment the following filetypes block.
+             -- WARN: THIS MAY CAUSE HIGHLIGHTING ISSUES WITHIN THE TEMPLATE SCOPE WHEN TSSERVER ATTACHES TO VUE FILES
 
-            -- filetypes = { "typescript", "javascript", "javascriptreact", "typescriptreact", "vue" },
-            init_options = {
-              plugins = {
-                {
-                  name = "@vue/typescript-plugin",
-                  location = volar_path,
-                  languages = { "vue" },
-                },
-              },
-            },
-          })
-        end,
+             -- filetypes = { "typescript", "javascript", "javascriptreact", "typescriptreact", "vue" },
+             init_options = {
+               plugins = {
+                 {
+                   name = "@vue/typescript-plugin",
+                   location = volar_path,
+                   languages = { "vue" },
+                 },
+               },
+             },
+           })
+         end,
                 ["gopls"] = function()
                   require("lspconfig").gopls.setup {
                     capabilities = capabilities,
