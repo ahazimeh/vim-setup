07ee611f566ea851f843af3be06243f92af0094d
Ali Hazimeh <ahazimeh@adio.gov.ae>
2025-02-03 06:52:41 +0200

    handle cs format files

--- lua/theprimeagen/lazy/prettier.lua
+++ lua/theprimeagen/lazy/prettier.lua
@@ -1,3 +1,14 @@
 return {
-    'sbdchd/neoformat'
+    'sbdchd/neoformat',
+    config = function()
+        -- Use LSP for .cs files, keep original Neoformat for others
+        vim.api.nvim_create_user_command("Neoformat", function()
+            local filetype = vim.bo.filetype
+            if filetype == "cs" then
+                vim.lsp.buf.format({ async = false })
+            else
+                vim.cmd('Neoformat')
+            end
+        end, { nargs = 0 })
+    end
 }
